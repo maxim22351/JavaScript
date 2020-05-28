81f62a75149225682aaf0924fd937cff5348e21a
Max <max.furman2014@gmail.com>
2020-05-28 14:09:42 +0300

    two_one

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-let one = false,
-    two = true;
+let ones = 1,
+    two = 85;
 
-console.log(one ||  two);     
+switch (ones) {
+    case ones == 1:
+        console.log('yes');
+        break;
+    case ones < 1:
+        console.log('no');
+        break; 
+    default: 
+      console.log(58);
+      break;     
+      
+
+}    
