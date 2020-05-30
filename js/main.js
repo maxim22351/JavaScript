6bae41746411c964da8226c9a953b6eac7f9289f
Max <max.furman2014@gmail.com>
2020-05-30 15:36:07 +0300

    Функции

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-for ( let i = 1; i < 15; i++){
-    if(i == 10){
-        break;
-    }
-    console.log(i);
-}
+// function num(a,b){
+//     return(a + b);
+// }
+
+// let one =(a,b) => a+b
+
+// console.log(one(3,5));
+
+// let two = "Hello"
+
+// console.log(two.length);
+
+// console.log(two.toLocaleUpperCase());
+// console.log(two.toLocaleLowerCase());
+
+
+let num = 15.9;
+
+console.log(Math.round(num));
+
+console.log(parseInt(num));
+console.log(parseFloat(num));
